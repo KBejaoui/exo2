80c8aaecc5bf18eb94f18e362de4e1df0c4846de
khadijabji <khbejaoui@laposte.net>
2020-03-28 23:12:00 +0100

    add

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ const mysql = require("mysql");
 const settings = require("./settings.json");
 
 const sqlConfig = settings.sqlConfig;
-
+    
 const app = express();
-app.listen(3000, () => {
-    console.log("SERVER STARTED");
+app.listen(3000, () =>  {
+    console.log("SERVER STARTED !");
 });
 
-app.use(express.static("./public/"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/api/articles", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
-
     sqlConnection.query(
         "SELECT id, title, content, author, created_at FROM node_articles ORDER BY id DESC LIMIT 5",
         (error, result) => {
             if (error) {
-                console.log("ERROR", error.code);
+                console.log("ERROR :", error.code);
             } else {
-                console.log("RESULT", result);
+                res.send(result);
+            }
+            sqlConnection.end();
+        }
+    );
+});
+
+app.get("/api/comments", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+    sqlConnection.query(
+        "SELECT id, article_id, author, content, created_at FROM node_comments ORDER BY id DESC LIMIT 5",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR :", error.code);
+            } else {
                 res.send(result);
             }
             sqlConnection.end();
@@ -30,124 +43,79 @@ app.get("/api/articles", (req, res) => {
 });
 
 app.route("/api/articles/create")
-    .get((req, res) => res.status(503).send({ status: "khadija" }))
+    .get((req, res) => res.status(503).send({status: "error"}))
     .post((req, res) => {
-
         console.log(req.body);
-
-        const articles_title = req.body.articles_title;
-        const articles_content = req.body.articles_content;
-        const articles_author_id = req.body.articles_author_id;
-        const articles_create_at = req.body.articles_create_at;
-
         const sqlConnection = mysql.createConnection(sqlConfig);
-
         sqlConnection.query(
             "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)",
-            [articles_title, articles_content, articles_author_id, articles_create_at],
+            [req.body.title, req.body.content, req.body.author, req.body.created_at],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR", error.code);
-                    res.status(503).send({ status: "ERROR" });
+                    console.log("error :", error.code);
+                    res.status(503).send({status: "error"});
                 } else {
-                    res.send({ status: "OK" });
                     console.log(result);
+                    res.send({status: "OK"});
                 }
                 sqlConnection.end();
-            }
-        );
+            });    
     });
 
-    app.route("/api/articles/delete")
+app.route("/api/articles/delete")
     .get((req, res) => res.status(503).send({ status: "ERROR" }))
     .post((req, res) => {
-
-        const articles_id = req.body.articles_id;
-
         const sqlConnection = mysql.createConnection(sqlConfig);
-
         sqlConnection.query(
             "DELETE FROM node_articles WHERE id = ?",
-            [articles_id],
+            [req.body.articleId],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR", error.code);
+                    console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
-                    res.send({ status: "OK" });
                     console.log(result);
+                    res.send({ status: "OK" });
                 }
                 sqlConnection.end();
-            }
-        );
+            });
     });
-
-   app.get("/api/comments", (req, res) => {
-    const sqlConnection = mysql.createConnection(sqlConfig);
-
-    sqlConnection.query(
-        "SELECT id, article_id, content, author, created_at FROM node_comments ORDER BY id DESC LIMIT 5",
-        (error, result) => {
-            if (error) {
-                console.log("ERROR", error.code);
-            } else {
-                console.log("RESULT", result);
-                res.send(result);
-            }
-            sqlConnection.end();
-        }
-    );
-});
 
 app.route("/api/comments/create")
     .get((req, res) => res.status(503).send({ status: "ERROR" }))
     .post((req, res) => {
-
         console.log(req.body);
-
-        const comments_article_id = req.body.comments_article_id;
-        const comments_content = req.body.comments_content;
-        const comments_author_id = req.body.comments_author_id;
-        const comments_create_at = req.body.comments_create_at;
-
         const sqlConnection = mysql.createConnection(sqlConfig);
-
         sqlConnection.query(
             "INSERT INTO node_comments VALUES (NULL, ?, ?, ?, ?)",
-            [comments_article_id, comments_content, comments_author_id, comments_create_at],
+            [req.body.article_id, req.body.author, req.body.content, req.body.created_at],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR", error.code);
+                    console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
-                    res.send({ status: "OK" });
                     console.log(result);
+                    res.send({ status: "OK" });
                 }
                 sqlConnection.end();
-            }
-        );
+            });
     });
 
-    app.route("/api/comments/delete")
+app.route("/api/comments/delete")
     .get((req, res) => res.status(503).send({ status: "ERROR" }))
     .post((req, res) => {
-
-        const comments_id = req.body.comments_id;
-
         const sqlConnection = mysql.createConnection(sqlConfig);
-
         sqlConnection.query(
             "DELETE FROM node_comments WHERE id = ?",
-            [comments_id],
+            [req.body.commentID],
             (error, result) => {
                 if (error) {
-                    console.log("ERROR", error.code);
+                    console.log("ERROR :", error.code);
                     res.status(503).send({ status: "ERROR" });
                 } else {
-                    res.send({ status: "OK" });
                     console.log(result);
+                    res.send({ status: "OK" });
                 }
                 sqlConnection.end();
-            }
-        );
+            });
     });
